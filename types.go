c4323e6c5d59d09831e4e30038aa24f49004c235
Senior Mobile app | Web | API Developer <66872038+kstar0102@users.noreply.github.com>
2023-11-28 11:28:35 +0200

    Revert "Revert "final fix and code clear""

---
+++ types.go
@@ -0,0 +1,19 @@
+package main
+
+type Config struct {
+	Messages struct {
+		LuckyDrawStart                             string `json:"luckyDrawStart" yaml:"luckyDrawStart"`
+		WillChooseNumberOfPersons                  string `json:"willChooseNumberOfPersons" yaml:"willChooseNumberOfPersons"`
+		WillChooseOnePerson                        string `json:"willChooseOnePerson" yaml:"willChooseOnePerson"`
+		WinnerIs                                   string `json:"winnerIs" yaml:"winnerIs"`
+		ThereAreNMembersJoined                     string `json:"thereAreNMembersJoined" yaml:"thereAreNMembersJoined"`
+		ThereIsOneMemberJoined                     string `json:"thereIsOneMemberJoined" yaml:"thereIsOneMemberJoined"`
+		NoOneJoined                                string `json:"noOneJoined" yaml:"noOneJoined"`
+		TimeLeftForJoin                            string `json:"timeLeftToJoin" yaml:"timeLeftToJoin"`
+		TheDrawIsOver                              string `json:"theDrawIsOver" yaml:"theDrawIsOver"`
+		TheDrawIsNotStartedYet                     string `json:"theDrawIsNotStartedYet" yaml:"theDrawIsNotStartedYet"`
+		TheDrawIsAlreadyStartedAndHasNotStoppedYet string `json:"theDrawIsAlreadyStartedAndHasNotStoppedYet" yaml:"theDrawIsAlreadyStartedAndHasNotStoppedYet"`
+		NotifyWinner                               string `json:"notifyWinner" yaml:"notifyWinner"`
+		AllMembersGotTheirPrize                    string `json:"allMembersGotTheirPrize" yaml:"allMembersGotTheirPrize"`
+	} `json:"messages" yaml:"messages"`
+}
